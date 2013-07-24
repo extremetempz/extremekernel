42bd07e8a4b91b863513fd41bc6377ebe96369e2
extremetempz <justinscollo1@gmail.com>
2013-07-24 14:55:17 +1000

    Readhead To 2048-256

--- include/linux/mm.h
+++ include/linux/mm.h
@@ -166,7 +166,7 @@ static inline int is_pfn_mapping(struct vm_area_struct *vma)
  */
 struct vm_fault {
 	unsigned int flags;		/* FAULT_FLAG_xxx flags */
-	pgoff_t pgoff;			/* Logical page offset based on vma */
+	pgoff_t pgoff;	m		/* Logical page offset based on vma */
 	void __user *virtual_address;	/* Faulting virtual address */
 
 	struct page *page;		/* ->fault handlers should return a
@@ -1170,8 +1170,8 @@ int write_one_page(struct page *page, int wait);
 void task_dirty_inc(struct task_struct *tsk);
 
 /* readahead.c */
-#define VM_MAX_READAHEAD	128	/* kbytes */
-#define VM_MIN_READAHEAD	16	/* kbytes (includes current page) */
+#define VM_MAX_READAHEAD	2048	/* kbytes */
+#define VM_MIN_READAHEAD	256	/* kbytes (includes current page) */
 
 int force_page_cache_readahead(struct address_space *mapping, struct file *filp,
 			pgoff_t offset, unsigned long nr_to_read);
